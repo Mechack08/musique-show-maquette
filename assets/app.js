6f3651ee8a4dd8325008e3a70d5a99d1667dfcda
8pro <mechackkobius@gmail.com>
2021-10-27 16:21:00 +0300

    Popular songs icon start play done

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,14 @@
 const popSongCards = document.querySelectorAll('.pop-song-card');
+const popCardSong = document.querySelectorAll('.pop-song-icon');
 
-popSongCards.forEach(popcard => {
-    const popCardSong = document.querySelector('.pop-song-icon');
-
+popSongCards.forEach((popcard, i) => {
     popcard.addEventListener('mouseenter', () => {
-       popCardSong.classList.add('pop-song-icon-show')
+        const icon = popCardSong[i];
+        icon.classList.add('pop-song-icon-show')
     })
+
     popcard.addEventListener('mouseleave', () => {
-        popCardSong.classList.remove('pop-song-icon-show')
+        const icon = popCardSong[i];
+        icon.classList.remove('pop-song-icon-show')
     })
 })
