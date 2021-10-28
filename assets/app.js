74bd9d9f6523b6807d7d2dbb198d48f3dc8bb178
8pro <mechackkobius@gmail.com>
2021-10-28 11:39:29 +0300

    Menu fixed added

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,16 @@
 const popSongCards = document.querySelectorAll('.pop-song-card');
 const popCardSong = document.querySelectorAll('.pop-song-icon');
+const menu = document.querySelector('.menu-container')
+
+window.addEventListener('scroll', () => {
+    const {scrollTop} = document.documentElement;
+    if(scrollTop >= 58){
+        menu.classList.add('menu-pos-fixed')
+    }
+    else{
+        menu.classList.remove('menu-pos-fixed')
+    }
+})
 
 popSongCards.forEach((popcard, i) => {
     popcard.addEventListener('mouseenter', () => {
